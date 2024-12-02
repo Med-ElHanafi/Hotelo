54e9b07104ab75b1ec77df2b1339b559ed54788b
elhanafi <mohamed.elhanafy@hagergroup-external.com>
2024-12-02 15:54:04 +0100

    Initial Commit

---
+++ Hotelo/HoteloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoteloApp.swift
+//  Hotelo
+//
+//  Created by Mohamed El hanafi on 02/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct HoteloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HoteloTests/HoteloTests.swift
@@ -0,0 +1,17 @@
+//
+//  HoteloTests.swift
+//  HoteloTests
+//
+//  Created by Mohamed El hanafi on 02/12/2024.
+//
+
+import Testing
+@testable import Hotelo
+
+struct HoteloTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
